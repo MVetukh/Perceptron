76f2c592f8c31da8d5cf828de219975c781cfdb6
Askalbe <92388142+Askalbe@users.noreply.github.com>
2023-11-28 20:15:50 +0300

    load

--- mnistTorch/mnisttorch.py
+++ mnistTorch/mnisttorch.py
@@ -1,17 +1,8 @@
+
 import numpy as np
 from matplotlib import pyplot as plt
 import torch
 import torch.optim as optim
-
-class Network(torch.nn.Modules):
-    def __init__(self):
-        super(Network, self).__init__()
-        self.fc1 = torch.nn.Linear(784, 34)
-        self.fc2 = torch.nn.Sigmoid(34, 10)
-
-    def forward(self, input):
-        output = torch.sigmoid(self.fc1(input))
-        output = self.fc2(output)
-        return output
+import torchvision
 
 
